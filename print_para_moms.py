0422b8d8f490c42ade7a61d257bb9689c061a76e
Yuki YAO <yaoxx366@umn.edu>
2018-03-07 18:22:34 -0600

    mainly changed print_para_moms

--- print_para_moms.py
+++ print_para_moms.py
@@ -1,6 +1,19 @@
-from SCEconomy_give_A import *
+import numpy as np
+from SCEconomy_bizluv_give_A import *
 
 e = import_econ()
+
+np.save('./save_data/data_i_s', e.data_i_s[:, -100:])
+np.save('./save_data/agrid', e.agrid)
+np.save('./save_data/bgrid', e.bgrid)
+np.save('./save_data/kapgrid', e.kapgrid)
+np.save('./save_data/epsgrid', e.epsgrid)
+np.save('./save_data/zgrid', e.zgrid)
+np.save('./save_data/prob', e.prob)
+np.save('./save_data/is_to_iz', e.is_to_iz)
+np.save('./save_data/is_to_ieps', e.is_to_ieps)
+np.save('./save_data/is_to_ib', e.is_to_ib)
+
 
 print('agrid')
 print(e.agrid)
@@ -14,10 +27,18 @@ print(e.zgrid)
 print('epsgrid')
 print(e.epsgrid)
 
+print('bgrid')
+print(e.bgrid)
+
 print('prob')
 print(e.prob)
 
 e.print_parameters()
 e.calc_moments()
+
+print('appendix')
+print('bgrid')
+print(e.bgrid)
+
 
 
