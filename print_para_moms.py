8a91a50497ec16b5abe0ef283c5bb8cdd308cade
Yuki YAO <yaoxx366@umn.edu>
2018-06-07 15:26:01 -0500

    added rho_z = 0.9 transition matrix

--- print_para_moms.py
+++ print_para_moms.py
@@ -1,18 +1,17 @@
 import numpy as np
-from SCEconomy_bizluv_give_A import *
+from SCEconomy_lifecycle_give_A import *
 
 e = import_econ()
 
-np.save('./save_data/data_i_s', e.data_i_s[:, -100:])
+#np.save('./save_data/data_i_s', e.data_i_s[:, -100:])
 np.save('./save_data/agrid', e.agrid)
-np.save('./save_data/bgrid', e.bgrid)
 np.save('./save_data/kapgrid', e.kapgrid)
 np.save('./save_data/epsgrid', e.epsgrid)
 np.save('./save_data/zgrid', e.zgrid)
 np.save('./save_data/prob', e.prob)
 np.save('./save_data/is_to_iz', e.is_to_iz)
 np.save('./save_data/is_to_ieps', e.is_to_ieps)
-np.save('./save_data/is_to_ib', e.is_to_ib)
+
 
 
 print('agrid')
@@ -27,18 +26,11 @@ print(e.zgrid)
 print('epsgrid')
 print(e.epsgrid)
 
-print('bgrid')
-print(e.bgrid)
-
 print('prob')
 print(e.prob)
 
 e.print_parameters()
 e.calc_moments()
-
-print('appendix')
-print('bgrid')
-print(e.bgrid)
 
 
 
